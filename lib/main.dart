54165a00a86c5c8976120c50cfdfb820017f9a9f
Brian Surface <iosmobilebrian@gmail.com>
2020-07-03 20:26:25 -0400

    added in visibility widget and additional methods

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class StoryPage extends StatefulWidget {
 }
 
 class _StoryPageState extends State<StoryPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,8 +58,10 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 1 made by user.
+                   setState(() {
+                     brain.nextStory(1);
+                   });
 
-                    brain.nextStory(1);
                   },
                   color: Colors.red,
                   child: Text(
@@ -75,20 +78,24 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
 
-                    brain.nextStory(2);
-                  },
-                  color: Colors.blue,
-                  child: Text(
+                child: Visibility(
+                  visible: brain.shouldBeVisible(),
+                  child: FlatButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        brain.nextStory(2);
+                      });
 
-                    brain.getChoice2(),
-                    style: TextStyle(
-                      fontSize: 20.0,
+                    },
+                    color: Colors.blue,
+                    child: Text(
+
+                      brain.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
@@ -101,6 +108,4 @@ class _StoryPageState extends State<StoryPage> {
   }
 }
 
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:flutter/cupertino.dart';
+
 import 'story.dart';
 
 class StoryBrain {
@@ -41,7 +43,7 @@ class StoryBrain {
         choice2: ''),
   ];
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
+
 
 
 
@@ -68,7 +70,7 @@ String getChoice2(){
   return _storyData[_storyNumber].choice2;
 }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
+
 
 
 
@@ -76,14 +78,78 @@ String getChoice2(){
 
 void nextStory(int choiceNumber){
 
+  switch(choiceNumber){
+
+    case 1 : {
+
+      if(_storyNumber == 0 || _storyNumber == 1){
+
+        _storyNumber = 2;
+
+        print(_storyNumber);
+      }else if(_storyNumber == 2){
+
+        _storyNumber = 5;
+      }else{
+
+        restart();
+      }
+
+
+
+      break;
+
+    }
+    case 2: {
+
+      if(_storyNumber == 0){
+
+        _storyNumber = 1;
+
+        print(_storyNumber);
+      }else if(_storyNumber == 1){
+
+        _storyNumber = 3;
+
+        print(_storyNumber);
+
+      }else if(_storyNumber == 2){
+
+        _storyNumber = 4;
+
+        print(_storyNumber);
+      }else{
+
+        restart();
+
+      }
+
+
+    }
+
+
+
+
+  }
+
 
 }
 
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
+void restart(){
 
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
+    _storyNumber = 0;
+}
 
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+bool shouldBeVisible(){
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  if(_storyNumber == 0 || _storyNumber ==1 || _storyNumber == 2){
+
+    return true;
+  }else{
+
+    return false;
+  }
+}
+
+
 }
